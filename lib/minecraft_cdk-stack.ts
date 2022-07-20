ca66858860de100a72f99fc8cfec30f94745cc19
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-07-21 00:06:24 +0900

    initial commit

--- lib/minecraft_cdk-stack.ts
+++ lib/minecraft_cdk-stack.ts
@@ -1,16 +1,58 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  CfnOutput,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+  Token,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { MachineImage } from "aws-cdk-lib/aws-ec2";
 
 export class MinecraftCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC作成
+    const vpc = new ec2.Vpc(this, "MinecraftVpc", {
+      maxAzs: 1,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          name: "PublicSubnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MinecraftCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // キーペア作成
+    const cfnKeyPair = new ec2.CfnKeyPair(this, "CfnKeyPair", {
+      keyName: "test-key-pair",
+    });
+    cfnKeyPair.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+    // キーペア取得コマンドアウトプット
+    new CfnOutput(this, "GetSSHKeyCommand", {
+      value: `aws ssm get-parameter --name /ec2/keypair/${cfnKeyPair.getAtt(
+        "KeyPairId"
+      )} --region ${
+        this.region
+      } --with-decryption --query Parameter.Value --output text`,
+    });
+
+    // EC2作成
+    const instance = new ec2.Instance(this, "MinecraftServer", {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T4G,
+        ec2.InstanceSize.SMALL
+      ),
+      machineImage: MachineImage.genericLinux({
+        "ap-northeast-1": "ami-0f808c98d829a4f79",
+      }),
+      keyName: Token.asString(cfnKeyPair.ref),
+    });
+    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(22));
+    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(25565));
   }
 }
